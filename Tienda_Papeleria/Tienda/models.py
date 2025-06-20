dbb4b8a411af77a39430ac8b73fd52d175098b84
Yast1n <yastingarroz00@gmail.com>
2025-06-20 15:24:40 -0400

    creacion modelos y config admin

--- Tienda_Papeleria/Tienda/models.py
+++ Tienda_Papeleria/Tienda/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.nombre
+    
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=200)
+    descripcion = models.TextField()
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    imagen = models.ImageField(upload_to='productos/')
+    stock = models.PositiveIntegerField(default=0)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nombre
+    
+
