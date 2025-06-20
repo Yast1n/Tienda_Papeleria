9f9aa8aa823026bd6a3df975f72f1a669782d238
Yast1n <yastingarroz00@gmail.com>
2025-06-20 15:56:36 -0400

    cambio todo home

--- Tienda_Papeleria/Tienda/views.py
+++ Tienda_Papeleria/Tienda/views.py
@@ -2,5 +2,21 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def index(request):
-    return render(request, 'Tienda/index.html')
+from .models import Categoria, Producto
+
+def home(request):
+    categorias = Categoria.objects.all()
+    productos_por_categoria = []
+
+    for categoria in categorias:
+        # Trae los primeros 3 productos de cada categoría (puedes cambiar el número)
+        productos = Producto.objects.filter(categoria=categoria)[:3]
+        if productos:  # Solo agrega si hay productos
+            productos_por_categoria.append({
+                'categoria': categoria,
+                'productos': productos,
+            })
+
+    return render(request, 'Tienda/home.html', {
+        'productos_por_categoria': productos_por_categoria,
+    })
